02103d0669c34a09320e91bd16aae6e9852a9220
Jason Saturno <saturno.jason@gmail.com>
2023-02-06 08:59:46 -0500

    Update Sbatch_IonQuant.sh

--- 4_IonQuant/Sbatch_IonQuant.sh
+++ 4_IonQuant/Sbatch_IonQuant.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=mghpcc-largemem 	#Queue to be used?
-#SBATCH --account=bch-mghpcc		#account name
+#SBATCH --partition=steen-compute 	#Queue to be used?
+#SBATCH --account=steen		#account name
 #SBATCH --time=239:59:00 		#12 hours. Normal search (i.e. tryptic, Oxidation and acetylation) will take +- 2 hours with 30 input files...
 #SBATCH --job-name=IonQuant 		#Job name
 #SBATCH --nodes=1			#Number of Nodes needed
 #SBATCH --cpus-per-task=96		#Number of CPUS needed
 #SBATCH --mem=360GB			#Memory needed	
+#SBATCH --qos=steen
 
 export LC_ALL=C
 
