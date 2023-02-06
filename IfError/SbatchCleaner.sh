0793e4ccc8f78fcbf0452d867a63f6d911444feb
Jason Saturno <saturno.jason@gmail.com>
2023-02-06 10:39:15 -0500

    Update SbatchCleaner.sh

--- IfError/SbatchCleaner.sh
+++ IfError/SbatchCleaner.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
 #Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-3"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
+sbatch -p steen-compute -w "compute-6-0"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-4"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
+sbatch -p steen-compute -w "compute-6-1"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
-#sbatch -p steen-compute -w "compute-m7c1-0-5"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
+#sbatch -p steen-compute -w "compute-6-2"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-6"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-7"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-8"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-9"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-10"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "compute-m7c1-0-11"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
+sbatch -p steen-compute -w "compute-6-3"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
 sbatch -p  steen-compute -w "gpu-m7c1-0-1"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
 sbatch -p steen-compute -w "gpu-m7c1-0-2"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
 
-rm /project/Path-steen/Patrick/MGHPCC/slurm*.out
+rm /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/slurm*.out
