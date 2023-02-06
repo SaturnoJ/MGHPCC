b9d166f402d1310c906fcc13b3431cd5c6821c01
Jason Saturno <saturno.jason@gmail.com>
2023-02-06 08:59:49 -0500

    Update Sbatch_proteinProphet_DIA.sh

--- 3_Philosopher/Sbatch_proteinProphet_DIA.sh
+++ 3_Philosopher/Sbatch_proteinProphet_DIA.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=mghpcc-short 	#Queue to be used?
-#SBATCH --account=bch-mghpcc		#account name
+#SBATCH --partition=steen-compute 	#Queue to be used?
+#SBATCH --account=steen		#account name
 #SBATCH --time=23:59:59 		#12 hours. Normal search (i.e. tryptic, Oxidation and acetylation) will take +- 2 hours with 30 input files...
 #SBATCH --job-name=ProteinProphet 		#Job name
 #SBATCH --nodes=1			#Number of Nodes needed
 #SBATCH --cpus-per-task=2		#Number of CPUS needed
 #SBATCH --mem=180GB			#Memory needed	
+#SBATCH --qos=steen
 
 export LC_ALL=C
 
