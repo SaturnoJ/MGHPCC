d4a19c0065e5c954d5cb003d7934c5d8aed1c879
Jason Saturno <saturno.jason@gmail.com>
2023-02-08 09:43:02 -0500

    Update SbatchCleaner.sh

--- IfError/SbatchCleaner.sh
+++ IfError/SbatchCleaner.sh
@@ -8,9 +8,6 @@ sbatch -p steen-compute -w "compute-6-1"  -n 1 -c 1  --mem=1MB /lab-share/Path-S
 sbatch -p steen-compute -w "compute-6-2"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
 #Will delete all old stuff everywhere.
 sbatch -p steen-compute -w "compute-6-3"  -n 1 -c 1  --mem=1MB /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p  steen-compute -w "gpu-m7c1-0-1"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
-#Will delete all old stuff everywhere.
-sbatch -p steen-compute -w "gpu-m7c1-0-2"  -n 1 -c 1  --mem=1MB /project/Path-Steen/Patrick/MGHPCC/deleteScript.sh
+
 
 rm /lab-share/Path-Steen-e2/Public/Jason/MGHPCC/slurm*.out
